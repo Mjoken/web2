02a912f581dafe334eea3d776433de37bb3b75e9
Oleg <oiksanov@yandex.ru>
2023-11-17 00:09:28 +0300

    the site has moved to django

--- web2/app1/urls.py
+++ web2/app1/urls.py
@@ -2,10 +2,15 @@ from django.urls import path, include
 from django.contrib import admin
 from .views import *
 
-urlpatterns = [
-    path('', index), ##'' значит переход при пустой страничке (первый аргумент '') переходит на этот адрес
+##'' значит переход при пустой страничке (первый аргумент '') переходит на этот адрес
     ##"Второй аргумент это функция для возврата реквеста"""
     ###НЕ ЗАБУДЬ В SETTING ПРОПИСАТЬ ДЛЯ МОДУЛЯ ПУТЬ В INSTALLED APPS####
-    path('index_add.html', index_add),
+
+urlpatterns = [
+    path('', index),
     path('index.html', index),
+    path('index_add.html', index_add),
+    path('index_del.html', index_del),
+    path('index_info.html', index_info),
+    path('index_auth.html', index_auth),
 ]
--- web2/app1/views.py
+++ web2/app1/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+
 ###ФУНКЦИИ ДЛЯ РЕКВЕСТОВ (СТРАНИЦЫ)###
 def index(request):
     return render(request, "html/index.html")
+
+
 def index_add(request):
     return render(request, "html/index_add.html")
+
+
+def index_del(request):
+    return render(request, "html/index_del.html")
+
+
+def index_auth(request):
+    return render(request, "html/index_auth.html")
+
+
+def index_info(request):
+    return render(request, "html/index_info.html")
