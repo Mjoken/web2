308ab57bc6bd5ccbcab86b52f864651d7d6667e9
Oleg <oiksanov@yandex.ru>
2023-11-17 22:41:18 +0300

    wip     bug hot-fix - wrong name of max_length in StudySession

--- web2/app1/models.py
+++ web2/app1/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class StudySession(models.Model):
-    group = models.CharField(max_lenght=10)
-    title = models.CharField(max_lenght=255)
-    short_title = models.CharField(max_lenght=10)
+    group = models.CharField(max_length=10)
+    title = models.CharField(max_length=255)
+    short_title = models.CharField(max_length=10)
     session_type = models.PositiveSmallIntegerField(default=0)
     subgroup = models.PositiveSmallIntegerField()
     comment = models.TextField(blank=True)
