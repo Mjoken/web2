355ecd75b3d0dc3245c61005991a9c8bd2c3bd1a
BedDuckey <artyomkos2002@yandex.ru>
2023-11-24 23:09:41 +0300

    WIP updating DB connection

--- web2/app1/models.py
+++ web2/app1/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class StudySession(models.Model):
     group = models.CharField(max_length=10)
     title = models.CharField(max_length=255)
-    short_title = models.CharField(max_length=10)
-    session_type = models.PositiveSmallIntegerField(default=0)
-    subgroup = models.PositiveSmallIntegerField()
-    comment = models.TextField(blank=True)
+    short_title = models.CharField(default="ПРДМТ",max_length=10, blank=True, null=True)
+    session_type = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+    subgroup = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
+    comment = models.TextField(default="Здесь могла быть ваша заметка", blank=True, null=True)
     time_start = models.DateTimeField()
     time_end = models.DateTimeField()
--- web2/app1/views.py
+++ web2/app1/views.py
@@ -8,43 +8,43 @@ def index(request):
 
 
 def index_addDel(request):
-    studySession = StudySession.objects.all()
-    return render(request, "html/index_addDel.html")
+    studysession = StudySession.objects.all()
+    return render(request, "html/index_addDel.html", {"studysession": studysession})
 
 # сохранение данных в бд
 def create(request):
     if request.method == "POST":
-        studySession = StudySession()
-        studySession.group = request.POST.get("group")
-        studySession.title = request.POST.get("title")
-        studySession.short_title = request.POST.get("short_title")
-        studySession.session_type = request.POST.get("session_type")
-        studySession.subgroup = request.POST.get("subgroup")
-        studySession.comment = request.POST.get("comment")
-        studySession.time_start = request.POST.get("time_start")
-        studySession.time_end = request.POST.get("time_end")
-        studySession.save()
+        studysession = StudySession()
+        studysession.group = request.POST.get("group")
+        studysession.title = request.POST.get("title")
+        studysession.short_title = request.POST.get("short_title")
+        studysession.session_type = request.POST.get("session_type")
+        studysession.subgroup = request.POST.get("subgroup")
+        studysession.comment = request.POST.get("comment")
+        studysession.time_start = request.POST.get("time_start")
+        studysession.time_end = request.POST.get("time_end")
+        studysession.save()
     return HttpResponseRedirect("/")
 
 
 # изменение данных в бд
 def edit(request, id):
     try:
-        studySession = StudySession.objects.get(id=id)
+        studysession = StudySession.objects.get(id=id)
 
         if request.method == "POST":
-            studySession.group = request.POST.get("group")
-            studySession.title = request.POST.get("title")
-            studySession.short_title = request.POST.get("short_title")
-            studySession.session_type = request.POST.get("session_type")
-            studySession.subgroup = request.POST.get("subgroup")
-            studySession.comment = request.POST.get("comment")
-            studySession.time_start = request.POST.get("time_start")
-            studySession.time_end = request.POST.get("time_end")
-            studySession.save()
+            studysession.group = request.POST.get("group")
+            studysession.title = request.POST.get("title")
+            studysession.short_title = request.POST.get("short_title")
+            studysession.session_type = request.POST.get("session_type")
+            studysession.subgroup = request.POST.get("subgroup")
+            studysession.comment = request.POST.get("comment")
+            studysession.time_start = request.POST.get("time_start")
+            studysession.time_end = request.POST.get("time_end")
+            studysession.save()
             return HttpResponseRedirect("/")
         else:
-            return render(request, "edit.html", {"studySession": studySession})
+            return render(request, "edit.html", {"studySession": studysession})
     except StudySession.DoesNotExist:
         return HttpResponseNotFound("<h2>StudySession not found</h2>")
 
@@ -52,8 +52,8 @@ def edit(request, id):
 # удаление данных из бд
 def delete(request, id):
     try:
-        studySession = StudySession.objects.get(id=id)
-        studySession.delete()
+        studysession = StudySession.objects.get(id=id)
+        studysession.delete()
         return HttpResponseRedirect("/")
     except StudySession.DoesNotExist:
         return HttpResponseNotFound("<h2>StudySession not found</h2>")
