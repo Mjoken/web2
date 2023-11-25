7fdf45605fe1e031d46728bd655fed1483d2eb23
Oleg <oiksanov@yandex.ru>
2023-11-25 09:14:39 +0300

    wip new small front hot-fix

--- web2/app1/models.py
+++ web2/app1/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class StudySession(models.Model):
     group = models.CharField(max_length=10)
     title = models.CharField(max_length=255)
-    short_title = models.CharField(default="ПРДМТ",max_length=10, blank=True, null=True)
+    short_title = models.CharField(default="Мат.Анализ", max_length=10, blank=True, null=True)
     session_type = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
     subgroup = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
     comment = models.TextField(default="Здесь могла быть ваша заметка", blank=True, null=True)
